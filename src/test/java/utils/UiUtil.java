e6e7e5660231c6ec9bdc6ba96646403179b5d2c3
yusupjanm <musajanyusupjan@yahoo.com>
2024-04-23 19:45:47 +0200

    uiutil

--- src/test/java/utils/UiUtil.java
+++ src/test/java/utils/UiUtil.java
@@ -1,15 +1,23 @@
 package utils;
 
+import net.datafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.Driver;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 public class UiUtil {
+    Actions actions;
+    Faker faker;
     public static WebElement getElement(String locator ){
         return Driver.getDriver().findElement(By.xpath(locator));
     }
@@ -57,5 +65,52 @@ public class UiUtil {
         Actions actions=new Actions(driver);
         actions.moveToElement(webElementA).moveToElement(webElementB).build().perform();
     }
+    public void scrollToElement(WebElement element){
+        actions.scrollToElement(element);
+    }
+
+    public UiUtil() {
+        faker = new Faker();
+    }
+
+    public String generateFakeFirstName() {
+        String firstName = faker.name().firstName();
+        return firstName;
+    }
+
+    public String generateFakeLastName() {
+        String lastName = faker.name().firstName();
+        return lastName;
+    }
+
+    public String generateEmail() {
+        String email = faker.internet().emailAddress();
+        return email;
+    }
+    public static int generateRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void waitForElementPresent(WebElement element){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void wait_(int second){
+        try {
+            Thread.sleep(second * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
