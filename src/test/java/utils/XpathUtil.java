5ba537645c88a3fc8ebacf772484435bda1755b4
yusupjanm <musajanyusupjan@yahoo.com>
2024-04-24 19:41:45 +0200

    xpath catalog

--- src/test/java/utils/XpathUtil.java
+++ src/test/java/utils/XpathUtil.java
@@ -32,6 +32,25 @@ public class XpathUtil {
     public static final String saveAndContinueEditButton="//*[@id='id_3601349b172a9082be048ce0d60cbe53']/following::span[7]";
     public static final String addCustomersSuccessMessage="//*[text()='The customer has been saved.']";
 
-
+// Catalog Xpath
+    public static final String addProductButton="//*[text()='Add Product']";
+    public static final String attributeSet="//*[@id='attribute_set_id']";
+    public static final String productType="//*[@id='product_type']";
+    public static final String continueButton="//*[text()='Continue']";
+    public static final String newProductName="//*[@id='name']";
+    public static final String newProductDescription="//*[@id='description']";
+    public static final String shortDescription="//*[@id='short_description']";
+    public static final String SKUField="//*[@id='sku']";
+    public static final String weightField="//*[@id='weight']";
+    public static final String setProductAsNewFromDate="//*[@id='news_from_date']";
+    public static final String setProductAsNewToDate="//*[@id='news_to_date']";
+    public static final String newProductStatus="//*[@id='status']";
+    public static final String URLKey="//*[@id='url_key']";
+    public static final String visibility="//*[@id='visibility']";
+    public static final String countryOfManufacture="//*[@id='country_of_manufacture']";
+    public static final String saveAndContinueEdit="//*[text()='Save and Continue Edit']";
+    public static final String newProductPrice="//*[@id='price']";
+    public static final String taxClass="//*[@id='tax_class_id']";
+    public static final String theProductHasBeenSaved="//*[text()='The product has been saved.']";
 
 }
