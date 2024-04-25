205a31f4a7e6691ac2ddfba0a0ea8096e2fa2d52
alirehimoglu <rehimogluali@gmail.com>
2024-04-25 18:28:48 +0300

    Marketing Module Xpath

--- src/test/java/tests/MarketingTest.java
+++ src/test/java/tests/MarketingTest.java
@@ -1,4 +1,25 @@
 package tests;
 
+import magento.modules.marketing.MarketingPage;
+import org.testng.annotations.Test;
+
 public class MarketingTest {
+
+    MarketingPage marketingPage = new MarketingPage();
+
+    @Test
+    public void addNewCatalogPriceRuleTest(){
+
+        marketingPage.managerCanAddNewCatalogPriceRule();
+
+    }
+
+    @Test
+    public void addNewCartPriceRuleTest(){
+
+        marketingPage.managerCanAddNewCartPriceRule();
+
+    }
+
+
 }
